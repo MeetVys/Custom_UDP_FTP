0a350a87ea81cbcd65a7f3b71b0616abaf80abfa
Meet Vyas <meetvyas682@gmail.com>
2022-02-14 12:22:46 +0530

    modified

--- Client_v1.py
+++ Client_v1.py
@@ -1,12 +1,31 @@
 from pprint import pprint
-import socket 
+from pydoc import cli
+import socket
+from struct import pack 
 import time
-import multiprocessing as mp
-from ctypes import c_bool
+import _thread
+import os
 
 PORT = 3000
 HOST = '127.0.0.5'
 BUFFER_SIZE = 1024
+PACKET_DATA_SIZE = 1024
+WINDOW_SIZE = 10
+
+#list of size window , stores the packets that are currenty being transmitted 
+# seq_number -> packet
+list_pack = dict()
+#list of size window , stores the sequence number of packet being sent by the respective thread
+# thread_number -> seq_number
+list_seq = dict() 
+list_ack = dict()
+
+file = open("m1.txt", "rb")
+file_sent = False 
+base = 0 
+next_seq = 0 
+socket_lock = _thread.allocate_lock() 
+control = True 
 
 client_socket_recv = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 client_socket_send = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)        
@@ -18,6 +37,24 @@ class custom_packet :
         self.syn = syn
         self.fin = fin
         self.data = data
+    def get_string(self):
+        temp = ("seq:"+str(self.seq_number)+"size:"+str(self.data_size)+"syn:"+str(self.syn)+"fin:"+str(self.fin)+"data:").encode() + self.data
+        return temp
+
+def get_packet (SYN, FIN, seq_number):
+    global file 
+    packet_obj  = custom_packet()
+    packet_obj.data_size = PACKET_DATA_SIZE
+    packet_obj.seq_number = seq_number
+    packet_obj.syn = SYN 
+    packet_obj.fin = FIN 
+    packet_obj.data = file.read(PACKET_DATA_SIZE)
+    list_seq[seq_number%WINDOW_SIZE] = packet_obj
+
+    return packet_obj
+    
+    
+
 
 
 def conn_est_timer(p , start_time): 
@@ -45,20 +82,51 @@ def conn_est():
             print("Failed")
             
     
+def sender(id) : 
+    global list_pack , list_seq, control 
+    while control:
+
+        while list_packet(list_seq(id)) Null:
+            send_lo
+            send( packet(list_seq(id)))
+            sleep()
+
+
+def  sender_main() :
+    for i in range(WINDOW_SIZE):
+        pckt = get_packet(0,0,i+1)
+        list_pack.add(i+1 , pckt)
+        list_seq( i+1 , i+1)
+        _thread.start_new_thread(sender, (i+1))
+    
+def recv():
+    sender_main()
+    while True
+        rcvd_pckt , _ = client_socket_recv.recvfrom(BUFFER_SIZE)
+    decode() 
+    seq() 
+    if seq in list_seq
+        find thread corresp to seq 
+        add packet to packet_list
+        list_seq(thread_id) <- update 
+        ++base
+    
+
+    for in list
+        join
+
+            # send packet
+            pckt = get_packet(0,0,next_seq)
+            pckt = pckt.get_string
+            client_socket_send.sendto(pckt,(HOST,PORT))
+            list_ack[next_seq%WINDOW_SIZE] = 0 
+            # call thread timer 
 
 
 
 
 
 
-
-base = 0 
-next_seq = 0 
-Window_Size = 10
-def sender_thread_function () :
-    file = open("rt1.txt","rb")
-    while True :
-        while next_seq < base + Window_Size :
             #  Make packet and store it in the temp
             # send packet
             # make thread which retransmits if timeout
